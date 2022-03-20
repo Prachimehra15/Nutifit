744a34bf95e70f3c9d3541a4d69838aa31414da7
Prachimehra15 <prachikmehra@gmail.com>
2022-03-20 17:37:56 +0530

    now complete app

--- lib/dietreq.dart
+++ lib/dietreq.dart
@@ -1,32 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'gender.dart';
-import 'phyact.dart';
-class Diereq extends StatefulWidget {
-  const Diereq({Key? key}) : super(key: key);
+import 'hw.dart';
+class Direq extends StatefulWidget {
+  const Direq({Key? key}) : super(key: key);
 
   @override
-  _DiereqState createState() {
-    return _DiereqState();
+  _DireqState createState() {
+    return _DireqState();
   }
 }
-
-class Tech
-{
-  String label;
-  Color color;
-  Tech(this.label, this.color);
+class Diet {
+  String name;
+  IconData icon;
+  bool isSelected;
+  Diet(this.name, this.icon, this.isSelected);
 }
 
-class _DiereqState extends State<Diereq> {
+class _DireqState extends State<Direq> {
+  List<Diet> diets = <Diet>[];
 
-  int selectedIndex =0;
-  List<Tech> _chipsList = [
-    Tech("I eat all food groups", Colors.white),
-    Tech("Vegan", Colors.white),
-    Tech("Vegetarian", Colors.white),
-    Tech("Non-Vegetarian", Colors.white)
-  ];
-
+  @override
+  void initState() {
+    super.initState();
+    diets.add(Diet("Veg", MdiIcons.foodVariant, false));
+    diets.add(Diet("Non-Veg", MdiIcons.fish, false));
+  }
   @override
   Widget build(BuildContext context) {
     final go = Material(
@@ -38,9 +37,11 @@ class _DiereqState extends State<Diereq> {
           minWidth: MediaQuery.of(context).size.width,
           onPressed: () {
             Navigator.push(
-                context,MaterialPageRoute(builder: (context)=> Phyact()) );
+                context, MaterialPageRoute(builder: (context) => Hw()));
           },
-          child: Text("Continue", textAlign: TextAlign.center,
+          child: Text(
+            "Continue",
+            textAlign: TextAlign.center,
             style: TextStyle(
               fontSize: 20,
               color: Color(0xffd417bc),
@@ -48,61 +49,94 @@ class _DiereqState extends State<Diereq> {
               fontStyle: FontStyle.italic,
             ),
           ),
-        )
-    );
+        ));
     return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          child: Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.all(70),
-            child: Column(
-              children: <Widget>[
-                Container(
-                  child: Text("Do you have any dietary requirement?",
-                      style: TextStyle(color: Color(0xffffffff),fontFamily:'Lora',fontSize: 30)
+        body: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(70),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    child: Text("What is your dietary requirement?",
+                        style: TextStyle(
+                            color: Color(0xffffffff),
+                            fontFamily: 'Lora',
+                            fontSize: 44)),
                   ),
-                ),
-                SizedBox(height: 60,),
-                Wrap(
-                  spacing: 10,
-                  direction: Axis.horizontal,
-                  children: techChips(),
-                ),
-                SizedBox(height: 100,),
-                go,
-              ],
+                  SizedBox(
+                    height: 60,
+                  ),
+                  Container(
+                    height: 200,
+                    child: GridView.builder(
+                        gridDelegate:SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 3.0,
+                          mainAxisSpacing: 3.0,
+                        ),
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: true,
+                        itemCount: diets.length,
+                        itemBuilder: (context, index) {
+                          return InkWell(
+                            splashColor: Colors.pinkAccent,
+                            onTap: () {
+                              setState(() {
+                                diets.forEach((gender) => gender.isSelected = false);
+                                diets[index].isSelected = true;
+                              });
+                            },
+                            child: CustomRadio(diets[index]),
+                          );
 
+                        }),
+                  ),
+                  SizedBox(
+                    height: 100,
+                  ),
+                  go,
+                ],
+              ),
             ),
           ),
-        ),
-      )
-
-
-    );
+        ));
   }
+}
+class CustomRadio extends StatelessWidget {
+  Diet _diet;
 
-  List<Widget> techChips () {
-    List<Widget> chips = [];
-    for (int i=0; i< _chipsList.length; i++) {
-      Widget item = Padding(
-        padding: const EdgeInsets.only(left:10, right: 5),
-        child: ChoiceChip(
-          label: Text(_chipsList[i].label),
-          labelStyle: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 20),
-          backgroundColor: Colors.lightBlueAccent,
-          selectedColor: Colors.deepPurple,
-          selected: selectedIndex == i,
-          onSelected: (bool value)
-          {
-            setState(() {
-              selectedIndex = i;
-            });
-          },
+  CustomRadio(this._diet);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: _diet.isSelected ? Colors.deepPurple : Colors.lightBlueAccent,
+      child: Container(
+        width: 100,
+        height: 50,
+        alignment: Alignment.center,
+        margin: const EdgeInsets.all(5.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              _diet.icon,
+              color: _diet.isSelected ? Colors.white : Colors.black,
+              size: 40,
+            ),
+            SizedBox(height: 10),
+            Text(
+              _diet.name,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color:_diet.isSelected ? Colors.white : Colors.black),
+            )
+          ],
         ),
-      );
-      chips.add(item);
-    }
-    return chips;
+      ),
+    );
   }
 }
